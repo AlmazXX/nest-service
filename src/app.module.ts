e134b7e2a0a538589b58316f2d231f90fbb47576
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2024-12-22 13:39:14 +0600

    feat: specified port for the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AlbumModule } from './album/album.module';
 import { ArtistModule } from './artist/artist.module';
+import config from './config/config';
 import { FavModule } from './fav/fav.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ load: [config] }),
     PrismaModule,
     UserModule,
     ArtistModule,
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaClientExceptionFilter } from './common/filters/prisma-client-exception.filter';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new PrismaClientExceptionFilter());
-  await app.listen(4000);
+  const port = app.get(ConfigService).get('port');
+  await app.listen(port);
 }
 bootstrap();
